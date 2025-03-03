b31d5bd85e62360b38203cae39bdd19642869cf8
Carlos-Cao <carlos.cao2@gmail.com>
2025-03-03 22:34:05 +1300

    feat: add Footer component

---
+++ nbascoreboard/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-red-700 text-white py-4">
+      <div className="container mx-auto text-center">
+        <a href="https://github.com/Carlos-Cao">
+          <p>&copy; {new Date().getFullYear()} Carlos-Cao</p>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
