519ce30ea75862be103c254307bfed67c49b699f
Carlos-Cao <carlos.cao2@gmail.com>
2025-03-04 22:49:48 +1300

    feat: add TeamList component to show current NBA teams

--- nbascoreboard/src/app/page.tsx
+++ nbascoreboard/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import TeamList from "../components/TeamList";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <TeamList />
       <Footer />
     </div>
   );
